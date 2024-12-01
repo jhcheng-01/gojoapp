889f4f02af4dc801f589e1545757c0e53b7ec316
jhcheng01 <103155083+jhcheng01@users.noreply.github.com>
2024-12-01 02:39:11 -0600

    shop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ import './App.css'
 import freaky from "./images/freaky.jpg"
 import paused_gif from "./images/freeze.png"
 import playing_gif from "./images/backshot.gif"
+
 import shop_icon from "./images/shop.png"
+import chill_guy from "./images/chill.png"
+import goku from "./images/goku.jpg"
 
 import useSound from 'use-sound';
 import boom_sfx from './sounds/boom.mp3';
@@ -27,6 +30,7 @@ function App()
   const [play_boom] = useSound(boom_sfx);
   const [shop_opened, toggle_shop] = useState(false);
 
+  /*
   useEffect(() => {
     let interval = null;
     interval = setInterval(() => {
@@ -47,7 +51,9 @@ function App()
     {
       set_src(paused_gif)
     }
+    return;
   });
+  */
 
   function handle_click()
   {
@@ -57,6 +63,18 @@ function App()
       play_boom();
       set_lct(time);
     }
+  }
+
+  function open_shop()
+  {
+    toggle_shop(true);
+    toggle_click(false);
+  }
+
+  function close_shop()
+  {
+    toggle_shop(false);
+    toggle_click(true);
   }
 
   function Stats()
@@ -75,6 +93,70 @@ function App()
       </>
     );
     
+  }
+
+  function buy_item(cost, extra_power, extra_aura)
+  {
+    if (count >= cost)
+    {
+      setCount(count - cost);
+      setPower(extra_power + power);
+      setAura(extra_aura + aura);
+    }
+  }
+
+  function Shop_Item(props)
+  {
+    return (
+      <>
+        <div className="shop-item">
+          <img className="square-image" src={props.img}/>
+          <h1 className="text-stroke">{props.name}</h1>
+          <p>{props.description}</p>
+          {props.power > 0 && <h3>power: +{props.power}</h3>}
+          {props.aura > 0 && <h3>power: +{props.aura}</h3>}
+          <button type="button" onClick={() => buy_item(props.cost, props.power, props.aura)}>
+            <h3>buy (cost: {props.cost})</h3>
+          </button>
+        </div>
+      </>
+    );
+  }
+
+  function Catalog()
+  {
+    return (
+    <div className="catalog">
+      <ul>
+        <Shop_Item
+          img = {freaky}
+          name = "extra freak"
+          cost = {50}
+          power = {1}
+          aura = {0}
+          description = "g-g-g-gyatt?!?"
+        />
+
+        <Shop_Item
+          img = {chill_guy}
+          name = "chill guy"
+          cost = {500}
+          power = {0}
+          aura = {20}
+          description = "yea"
+        />
+
+        <Shop_Item
+          img = {goku}
+          name = "goku"
+          cost = {100000}
+          power = {1000}
+          aura = {0}
+          description = "i hear your brainrot is strong! let me fight it!"
+        />
+      </ul>
+    </div>
+    )
   }
 
   function Shop()
@@ -97,6 +179,13 @@ function App()
           <div className="center">
             <Stats/>
           </div>
+
+          <Catalog/>
+
+          <button type="button" onClick={() => close_shop()}>
+            <h3>close shop</h3>
+          </button>
+
         </div>
       </>
     )
@@ -136,7 +225,7 @@ function App()
           
           <Stats/>
           <div className="shop-button">
-            <button type="button" onClick={() => toggle_shop(true)}>
+            <button type="button" onClick={() => open_shop()}>
               <img src={shop_icon} alt="Shop" width="100" height="100"></img>
             </button>
           </div>
